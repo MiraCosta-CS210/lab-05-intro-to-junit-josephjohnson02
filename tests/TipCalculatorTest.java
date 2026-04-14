264520261c3b37d18ad36778e2d4d612ecc94f4c
Joseph Johnson <thatxenthos@gmail.com>
2026-04-14 14:42:08 -0700

    Completed Lab

--- tests/TipCalculatorTest.java
+++ tests/TipCalculatorTest.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TipCalculatorTest {
+    @Test
+    void calculateTipTest1() {
+        TipCalculator tipTest1 = new TipCalculator();
+        double tipAnswer = tipTest1.calculateTip(100, 20);
+        assertEquals(20, tipAnswer);
+    }
+
+    @Test
+    void calculateTipTest2() {
+        TipCalculator tipTest2 = new TipCalculator();
+        double tipAnswer = tipTest2.calculateTip(20, 50);
+        assertEquals(10, tipAnswer);
+    }
 }
